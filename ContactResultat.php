9ed954be1e14a91f00e5aa8dbd90402f49085506
SeverineSchiltz <49366997+SeverineSchiltz@users.noreply.github.com>
2020-05-21 17:10:14 +0200

    Add files via upload

--- ContactResultat.php
+++ ContactResultat.php
@@ -1,5 +1,5 @@
 
 
-<p>Bonjour <?php echo $_POST['prenom']; ?> ! Merci pour ton inscription!</p>
+<p>Bonjour <?php echo $_POST['prenom']; ?> ! Merci de vouloir t'inscrire!</p>
 
-<p>Si tu veux modifier tes références, <a href="contact.html">clique ici</a> pour revenir à la page formulaire.</p>
+<p>Pour le moment, le site est en construction et tes références n'ont pas été encodées. Merci de revenir plus tard. <a href="contact.html">Clique ici</a> pour revenir à la page formulaire.</p>
